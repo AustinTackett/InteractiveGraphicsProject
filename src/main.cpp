04ecb7d4c376c066c5d6e44df4d6f8a95e508d1c
AustinTackett <117427861+AustinTackett@users.noreply.github.com>
2026-01-23 01:32:56 -0700

    Refactor for Readability
    Refactored codebase to make it easier to read and use more cyGL functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,55 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "cyCore.h"
+#include "cyGL.h"
 #include "cyTriMesh.h"
 #include "cyMatrix.h"
-#include "cyQuat.h"
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cmath>
-#include <limits>
 
-#define V_SHADER_PATH "res/shaders/vertShader.glsl"
-#define F_SHADER_PATH "res/shaders/fragShader.glsl"
+#define V_SHADER_PATH "res/shaders/vertShader.vert"
+#define F_SHADER_PATH "res/shaders/fragShader.frag"
+
+/* Convert Degrees To Radians */
+float deg2Rad(float deg) { return deg * (cy::Pi<float>()/180.0f); }
 
 /* GLFW user input callbacks wrapper */
 class UserIO {
     public:
         inline static bool leftMouseHeld = false;
         inline static bool rightMouseHeld = false;
+        inline static double xMousePosDelta = 0.0;
+        inline static double yMousePosDelta = 0.0;
+        inline static double xRelativeMousePosDelta = 0.0;
+        inline static double yRelativeMousePosDelta = 0.0;        
+        inline static double xMousePos = 0.0;
+        inline static double yMousePos = 0.0;
+
+        static void Init(GLFWwindow* window)
+        {
+            _window = window;
+            glfwSetKeyCallback(window, getKeyInput);
+            glfwSetCursorPosCallback(window, getCursorPos);
+            glfwSetMouseButtonCallback(window, getMouseButton);
+        }
+        static void PollMousePos() 
+        {
+            glfwGetCursorPos(_window, &xMousePos, &yMousePos); 
+            glfwGetWindowSize(_window, &_windowWidth, &_windowHeight);
+            xMousePosDelta = xMousePos - _xMousePosLast; 
+            yMousePosDelta = yMousePos - _yMousePosLast;
+            xRelativeMousePosDelta = xMousePosDelta / _windowWidth;
+            yRelativeMousePosDelta = yMousePosDelta / _windowHeight;
+            _xMousePosLast = xMousePos;
+            _yMousePosLast = yMousePos;   
+        }
+
+    private:
+        inline static GLFWwindow* _window = nullptr;
+        inline static double _xMousePosLast = 0.0;
+        inline static double _yMousePosLast = 0.0;
+        inline static int _windowHeight;
+        inline static int _windowWidth;
 
         static void getCursorPos(GLFWwindow* window, double xMousePos, double yMousePos) { }
 
@@ -29,77 +61,88 @@ class UserIO {
             if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) { rightMouseHeld = true; }
             else { rightMouseHeld = false; }
         }
-
         static void getKeyInput(GLFWwindow *window, int key, int scancode, int action, int mods)
         {
-            if (key == GLFW_KEY_ESCAPE) { 
-                glfwSetWindowShouldClose(window, true); 
-            }
+            if (key == GLFW_KEY_ESCAPE) { glfwSetWindowShouldClose(window, true); }
         }
 };
 
-class RandomFloat {
+class Camera{
     public:
-        RandomFloat() 
+        float radius = 3.0;
+        float radialSpeed = 2.0;
+        cy::Vec3f worldX = cy::Vec3f(1, 0, 0);
+        cy::Vec3f worldY = cy::Vec3f(0, 1, 0);
+        cy::Vec3f worldZ = cy::Vec3f(0, 0, 1);
+        cy::Matrix4f currentOrientation = cy::Matrix4f::Identity();
+        cy::Matrix4f currentPosition = cy::Matrix4f::Identity();
+    
+        void update(float phiDelta, float thetaDelta, float radiusDelta)
         {
-            std::srand(1);
+            cy::Vec3f thetaDirection = cy::Matrix3f(currentOrientation) * worldX;
+            cy::Vec3f phiDirection = worldY;
+            cy::Matrix4f rotationTheta = cy::Matrix4f::Rotation(thetaDirection, thetaDelta);
+            cy::Matrix4f rotationPhi = cy::Matrix4f::Rotation(phiDirection, phiDelta);
+            currentOrientation = rotationPhi * rotationTheta * currentOrientation;
+
+            radius = cy::Max(0.0f, radialSpeed*radiusDelta + radius);
+            cy::Vec3f radiusDirection = cy::Matrix3f(currentOrientation) * worldZ;
+            currentPosition = cy::Matrix4f::Translation(radius*radiusDirection);
         }
+        cy::Matrix4f getViewMatrix()
+        {
+            return (currentPosition * currentOrientation).GetInverse();
+        }
+};
+
+class AnimatedRGB {
+    public:
+        const double animationDuration = 1.0;
+        double timeElapsed = 0.0;
+        double animationStartTime = 0.0;
+        float r = 1.0f;
+        float g = 0.0f;
+        float b = 0.0f;
+
+        AnimatedRGB(int seed = 1) { std::srand(seed); }
 
         // Generate random float between 0 and 1
         float getRandomFloat()
         {
             return static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX);
         }
-
+        void pollAnimation()
+        {
+            if (timeElapsed > animationDuration) 
+            {
+                animationStartTime = glfwGetTime();
+                r = getRandomFloat();
+                g = getRandomFloat();
+                b = getRandomFloat();
+            }
+            timeElapsed = glfwGetTime() - animationStartTime;
+        }
 };
-
-/* Reading Glsl Files */
-bool readFile(const std::string filePath, std::string& outString)
-{
-    /* Open File */
-    std::ifstream file(filePath);    
-    if (!file.is_open())
-    {
-        return false;
-    }
-
-    /* Read From File */
-    std::string tempStringLine; 
-    outString.clear();
-    while(std::getline(file, tempStringLine))
-    {
-        outString += tempStringLine;
-        outString += '\n';
-    }
-
-    return true; 
-}
-
-/* Convert Degrees To Radians */
-float deg2Rad(float deg)
-{
-    return deg * (cy::Pi<float>()/180.0f);
-}
 
 int main(int argc, char** argv) 
 {
     /* Parse Command Line Arguements */
-    const char* const OBJ_PATH = argv[1];
     if(argc != 2) 
     {
         std::cerr << "A single argument of a path to a obj file is expected" << std::endl;
         return -1;
     }
+    std::string objFilePath(argv[1]);
 
-    /* Initialize a GLFW window */
-    int glfwErr = glfwInit();
-    if (GLFW_TRUE != glfwErr)
+    /* Initialize a GLFW Window */
+    int glfwErrorCode = glfwInit();
+    if (GLFW_TRUE != glfwErrorCode)
     {
         std::cerr << "Could not initialize GLFW" << std::endl;
         return -1;
     }
 
-    /* Create a GLFW window */
+    /* Create a GLFW Window */
     int windowWidth = 1280;
     int windowHeight = 960;
     GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "InteractiveGraphicsProject", NULL, NULL);
@@ -110,254 +153,108 @@ int main(int argc, char** argv)
     }
     glfwMakeContextCurrent(window);
 
-    /* Initialize GLFW keyboard callbacks */
-    double xMousePos = 0;
-    double yMousePos = 0;
-    glfwSetKeyCallback(window, UserIO::getKeyInput);
-    glfwSetCursorPosCallback(window, UserIO::getCursorPos);
-    glfwSetMouseButtonCallback(window, UserIO::getMouseButton);
-
-    /* Initialize GLEW for using OpenGL functions */
-    GLenum glewErr = glewInit();
-    if (GLEW_OK != glewErr) 
-    {
+    /* Initialize GLEW for using OpenGL Functions */
+    GLenum glewErrorCode = glewInit();
+    if (GLEW_OK != glewErrorCode) {
         std::cerr << "Could not initialize GLEW" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Load Obj File Specified and Store the Vertices */
-    cy::TriMesh mesh;
-    bool meshErr = mesh.LoadFromFileObj(OBJ_PATH);
-    if (!meshErr)
+    /* Load Shaders */
+    cy::GLSLProgram program;
+    bool shaderIsReady = program.BuildFiles(V_SHADER_PATH, F_SHADER_PATH);
+    if (!shaderIsReady)
     {
-        std::cerr << "Could not load mesh from obj file at path " << OBJ_PATH << std::endl;
+        glfwTerminate();
+        return -1;          
+    }
+
+    /* Parse Mesh from File and Prepare Model Matrix from Mesh Bounding Box */
+    cy::TriMesh mesh;
+    bool meshIsReady = mesh.LoadFromFileObj(objFilePath.c_str());
+    if (!meshIsReady)
+    {
+        std::cerr << "Could not load mesh from obj file at path " << objFilePath << std::endl;
         glfwTerminate();
         return -1;        
     }
     mesh.ComputeBoundingBox();
+    cy::Vec3f meshCenter = (mesh.GetBoundMax() + mesh.GetBoundMin())/2;
+    cy::Matrix4f meshScale = cy::Matrix4f::Scale(cy::Vec3f(0.05f, 0.05f, 0.05f));
+    cy::Matrix4f meshRotationX = cy::Matrix4f::RotationX(deg2Rad(-90));
+    cy::Matrix4f meshRotationY = cy::Matrix4f::RotationY(deg2Rad(90));
+    cy::Matrix4f meshToOrigin = cy::Matrix4f::Translation(-meshCenter);
+    cy::Matrix4f modelMatrix = meshRotationY * meshRotationX * meshScale * meshToOrigin;
 
-    /* Create Vertex Buffer Object and Vertex Array Object  */
-    GLuint vBuf, vArr;
-    GLsizei numVBufs = 1;
-    GLsizei numVArrs = 1;
-    glGenBuffers(numVBufs, &vBuf); 
-    glGenVertexArrays(numVArrs, &vArr);
-    
-    glBindVertexArray(vArr);
-    glBindBuffer(GL_ARRAY_BUFFER, vBuf);
-    glBufferData(GL_ARRAY_BUFFER, mesh.NV()*sizeof(cy::Vec3f), &mesh.V(0), GL_STATIC_DRAW);
-
-    GLuint vAttribIdx = 0;
-    GLuint numVComponents = 3;
-    const void* vBuffStartOffset = 0;
-    glVertexAttribPointer(vAttribIdx, numVComponents, GL_FLOAT, GL_FALSE, sizeof(cy::Vec3f), vBuffStartOffset);
-    glEnableVertexAttribArray(vAttribIdx);
-
-    /* Read Vertex Shaders Code From Files */
-    std::string vShaderString;
-    bool vShaderFileErr = readFile(V_SHADER_PATH, vShaderString);
-    if (!vShaderFileErr)
-    {
-        std::cerr << "Could not load vertex shader at path " << V_SHADER_PATH << std::endl;
-        glfwTerminate();
-        return -1;   
-    }
-
-    /* Read Fragment Shaders Code From Files */
-    std::string fShaderString;
-    bool fShaderFileErr = readFile(F_SHADER_PATH, fShaderString);
-    if (!vShaderFileErr)
-    {
-        std::cerr << "Could not load fragment shader at path " << F_SHADER_PATH << std::endl;
-        glfwTerminate();
-        return -1;   
-    }
-
-    /* Compile Vertex Shaders */
-    GLuint vShader;
-    vShader = glCreateShader(GL_VERTEX_SHADER);
-
-    const GLchar* vshaderCharArray = vShaderString.c_str();
-    glShaderSource(vShader, 1, &vshaderCharArray, NULL);
-
-    GLint vshaderCompileErr;
-    const unsigned int vInfoLogLength = 1024;
-    char vShaderInfolog[vInfoLogLength];
-
-    glCompileShader(vShader);
-    glGetShaderiv(vShader, GL_COMPILE_STATUS, &vshaderCompileErr);
-    if (!vshaderCompileErr) 
-    {
-        glGetShaderInfoLog(vShader, vInfoLogLength, NULL, &vShaderInfolog[0]);
-        std::cerr << "Could not compile vertex shaders\n" << vShaderInfolog << std::endl;
-
-        glDeleteShader(vShader);
-        glfwTerminate();
-        return -1;     
-    }
-
-    /* Compile Fragment Shaders */
-    GLuint fShader;
-    fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-    const GLchar* fShaderCharArray = fShaderString.c_str();
-    glShaderSource(fShader, 1, &fShaderCharArray, NULL);
-
-    GLint fshaderCompileErr;
-    const unsigned int fInfoLogLength = 1024;
-    char fShaderInfolog[fInfoLogLength];
-
-    glCompileShader(fShader);
-    glGetShaderiv(fShader, GL_COMPILE_STATUS, &fshaderCompileErr);
-    if (!fshaderCompileErr) 
-    {
-        glGetShaderInfoLog(fShader, fInfoLogLength, NULL, &fShaderInfolog[0]);
-        std::cerr << "Could not compile fragment shaders\n" << fShaderInfolog << std::endl;
-        
-        glDeleteShader(vShader);
-        glDeleteShader(fShader);
-        glfwTerminate();
-        return -1;     
-    }
-
-    /* Link Shaders to OpenGL program */
-    GLuint program = glCreateProgram();
-    GLint linkProgramErr;
-
-    const unsigned int programInfoLogLength = 1024;
-    char programInfolog[programInfoLogLength];
-
-    glAttachShader(program, vShader);
-    glAttachShader(program, fShader);
-    glLinkProgram(program);
-    glGetProgramiv(program, GL_LINK_STATUS, &linkProgramErr);
-    if (!linkProgramErr)
-    {
-        glGetProgramInfoLog(program, programInfoLogLength, NULL, &programInfolog[0]);
-        std::cerr << "Could not link opengl program\n" << programInfolog << std::endl;
-
-        glDeleteShader(vShader);
-        glDeleteShader(fShader);
-        glDeleteProgram(program);
-        glfwTerminate();
-        return -1;      
-    }
-
-    /* Detach and delete shader objects after they are contained in the program */
-    glDetachShader(program, vShader);
-    glDetachShader(program, fShader);
-    glDeleteShader(vShader);
-    glDeleteShader(fShader);
-
-    /* Animation Params */
-    RandomFloat randGen;
-    const double animationDuration = 1.0;
-    double timeElapsed = 0.0;
-    double animationStartTime = 0.0;
-    float r = 1.0f;
-    float g = 0.0f;
-    float b = 0.0f;
+    /* Write Mesh to GPU */
+    GLuint posLocation = 0;
+    const void* posOffset = 0;
+    GLuint vao;
+    GLuint vbo;
+    GLenum usage = GL_STATIC_DRAW;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo); 
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, mesh.NV()*sizeof(cy::Vec3f), &mesh.V(0), usage);
+    glVertexAttribPointer(posLocation, 3, GL_FLOAT, GL_FALSE, sizeof(cy::Vec3f), posOffset);
+    glEnableVertexAttribArray(posLocation);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);  
 
     /* Execute GLFW Window */
-    double lastMousePosX = 0.0;
-    double lastMousePosY = 0.0;
-    double relativeMouseDeltaX = 0.0;
-    double relativeMouseDeltaY = 0.0f;
+    float thetaDelta = 0.0f;
+    float phiDelta = 0.0f;
+    float radiusDelta = 0.0f;
 
-    float angleThetaDelta = 0.0f;
-    float anglePhiDelta = 0.0f;
-    
-    float currentRadius = 2.0;
-    float radialSpeed = 2.0;
-    cy::Matrix4f currentOrientation = cy::Matrix4f::Identity();
+    float fovRadians = deg2Rad(75);
+    float zFar = 1000;
+    float zNear = 0.1f;
 
-    cy::Vec3f worldX(1, 0, 0);
-    cy::Vec3f worldY(0, 1, 0);
-    cy::Vec3f worldZ(0, 0, 1);
-
+    Camera camera;
+    AnimatedRGB animatedRgb;
+    UserIO::Init(window);
     glViewport(0, 0, windowWidth, windowHeight);
     glEnable(GL_DEPTH_TEST);
 
     while (!glfwWindowShouldClose(window))
-    {
-        /* Animate Background */
-        if (timeElapsed > animationDuration) 
-        {
-            animationStartTime = glfwGetTime();
-            r = randGen.getRandomFloat();
-            g = randGen.getRandomFloat();
-            b = randGen.getRandomFloat();
-        }
-        timeElapsed = glfwGetTime() - animationStartTime;
-        
-        /* Ready Window To Render Obj */
-        glUseProgram(program);
-        
-        //glClearColor(r, g, b, 1.0f);
-        glClearColor(0, 0, 0, 1.0);
+    {        
+        // Animated Background (disabled b/c distracting)        
+        //animatedRgb.pollAnimation();
+        //glClearColor(animatedRgb.r, animatedRgb.g, animatedRgb.b, 1.0); 
+
+        glClearColor(0, 0, 0, 1.0); 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        /* Get Cursor Position */
-        glfwGetCursorPos(window, &xMousePos, &yMousePos); 
-        relativeMouseDeltaX = (xMousePos - lastMousePosX) / windowWidth;  
-        relativeMouseDeltaY = (yMousePos - lastMousePosY) / windowHeight;
-        lastMousePosX = xMousePos;
-        lastMousePosY = yMousePos;
-        
-        /* Calculate Final MVP Matrix */
-
-        // Model Matrix
-        cy::Vec3f meshCenter = (mesh.GetBoundMax() + mesh.GetBoundMin())/2;
-        cy::Matrix4f meshScale = cy::Matrix4f::Scale(cy::Vec3f(0.05f, 0.05f, 0.05f));
-        cy::Matrix4f meshRotationX = cy::Matrix4f::RotationX(deg2Rad(-90));
-        cy::Matrix4f meshRotationY = cy::Matrix4f::RotationY(deg2Rad(90));
-        cy::Matrix4f meshToOrigin = cy::Matrix4f::Translation(-meshCenter);
-        
-        cy::Matrix4f modelMatrix = meshRotationY * meshRotationX * meshScale * meshToOrigin;
-
         // View Matrix
-        angleThetaDelta = 0.0f;
-        anglePhiDelta = 0.0f;
+        UserIO::PollMousePos();
+        thetaDelta = 0.0f;
+        phiDelta = 0.0f;
+        radiusDelta = 0.0f;
         if (UserIO::leftMouseHeld) 
         {
-            angleThetaDelta = static_cast<float>(relativeMouseDeltaY)*deg2Rad(360);
-            anglePhiDelta = static_cast<float>(relativeMouseDeltaX)*deg2Rad(360);
+            thetaDelta = static_cast<float>(UserIO::yRelativeMousePosDelta)*(2*cy::Pi<float>());
+            phiDelta = static_cast<float>(UserIO::xRelativeMousePosDelta)*(2*cy::Pi<float>());
         }
         if (UserIO::rightMouseHeld)
         {
-            currentRadius += radialSpeed * static_cast<float>(relativeMouseDeltaY);
-            currentRadius = std::max(0.0f, currentRadius);
+            radiusDelta = static_cast<float>(UserIO::yRelativeMousePosDelta);
         }        
-        cy::Vec3f thetaDirection = cy::Matrix3f(currentOrientation) * worldX;
-        cy::Vec3f phiDirection = worldY;
-
-        cy::Matrix4f rotationTheta = cy::Matrix4f::Rotation(thetaDirection, angleThetaDelta);
-        cy::Matrix4f rotationPhi = cy::Matrix4f::Rotation(phiDirection, anglePhiDelta);
-        currentOrientation = rotationPhi * rotationTheta * currentOrientation;
-
-        cy::Vec3f radiusDirection = cy::Matrix3f(currentOrientation) * worldZ;
-        cy::Matrix4f translationRadius = cy::Matrix4f::Translation(currentRadius * radiusDirection);
-        
-        cy::Matrix4f viewMatrix = (translationRadius * currentOrientation).GetInverse();
+        camera.update(phiDelta, thetaDelta, radiusDelta);
+        cy::Matrix4f viewMatrix = camera.getViewMatrix();
         
         // Perspective Matrix
         float aspect = static_cast<float>(windowWidth) / static_cast<float>(windowHeight);
-        float fovRadians = deg2Rad(75);
-        float zFar = 1000;
-        float zNear = 0.1f;
         cy::Matrix4f perspectiveMatrix = cy::Matrix4f::Perspective(fovRadians, aspect, zNear, zFar);
 
         // Final MVP Passed as Uniform Value to Shaders
         cy::Matrix4f mvp = perspectiveMatrix * viewMatrix * modelMatrix;
+        program.SetUniformMatrix4("mvp", &mvp.cell[0]);
 
-        int numMvps = 1;           
-        bool mvpTranspose = false; // Would be true if cy::Matrix was row major instead
-        GLint mvpLocation = glGetUniformLocation(program, "mvp");
-        glUniformMatrix4fv(mvpLocation, numMvps, mvpTranspose, &mvp.cell[0]);
-
-        /* Draw Obj */
-        glBindVertexArray(vArr);
+        /* Bind Program and Draw */
+        program.Bind();
+        glBindVertexArray(vao);
         glDrawArrays(GL_POINTS, 0, mesh.NV());
 
         /* Display Final Render */
